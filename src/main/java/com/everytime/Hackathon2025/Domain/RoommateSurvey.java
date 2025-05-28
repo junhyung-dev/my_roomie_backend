72f68e7b8f7490e29d34c11f127d329c6317db7e
min611 <net678600@gmail.com>
2025-05-28 23:27:22 +0900

    Update RoommateSurvey.java
    (수면, 기상, 샤워, 전화) 시간 추가

--- src/main/java/com/everytime/Hackathon2025/Domain/RoommateSurvey.java
+++ src/main/java/com/everytime/Hackathon2025/Domain/RoommateSurvey.java
@@ -1,12 +1,3 @@
-package com.everytime.Hackathon2025.Domain;
-
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
-
-
 @Entity
 @NoArgsConstructor
 @Getter
@@ -29,20 +20,35 @@ public class RoommateSurvey {
     @Column(nullable = false)
     private boolean smoking;
 
-    @Column(length = 500) //기타
+    @Column(length = 500)
     private String etc;
 
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
-    public RoommateSurvey(User user, String dormName, String cleanLevel, boolean smoking, String etc) {
+    @Column(name = "sleep_time")
+    private String sleepTime;
+
+    @Column(name = "wake_up_time")
+    private String wakeUpTime;
+
+    @Column(name = "phone_time")
+    private String phoneTime;
+
+    @Column(name = "shower_time")
+    private String showerTime;
+
+    public RoommateSurvey(User user, String dormName, String cleanLevel, boolean smoking, String etc,
+                          String sleepTime, String wakeUpTime, String phoneTime, String showerTime) {
         this.user = user;
         this.dormName = dormName;
         this.cleanLevel = cleanLevel;
         this.smoking = smoking;
         this.etc = etc;
+        this.sleepTime = sleepTime;
+        this.wakeUpTime = wakeUpTime;
+        this.phoneTime = phoneTime;
+        this.showerTime = showerTime;
         this.createdAt = LocalDateTime.now();
-
     }
-
 }
